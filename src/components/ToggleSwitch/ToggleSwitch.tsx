767105bd48f7d8df5cdcaa6098d9b7de4f3e9b8e
Gianluca Fuoco <gianluca_1227@hotmail.com>
2022-02-14 17:45:48 -0500

    Created ToggleSwitch component to act as a toggle for dark/light mode

---
+++ src/components/ToggleSwitch/ToggleSwitch.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./ToggleSwitch.scss";
+
+interface Props {
+  top?: number;
+  right?: number;
+  left?: number;
+  bottom?: number;
+  handleClick?: () => void;
+}
+
+const ToggleSwitch: React.FC<Props> = ({
+  top,
+  right,
+  left,
+  bottom,
+  handleClick,
+}) => {
+  return (
+    <div
+      className="toggle"
+      style={{
+        top,
+        right,
+        left,
+        bottom,
+      }}
+    >
+      <input type="checkbox" id="toggle" className="check" />
+      <label
+        htmlFor="toggle"
+        className="toggle-label"
+        onClick={handleClick}
+      ></label>
+    </div>
+  );
+};
+
+export default ToggleSwitch;
